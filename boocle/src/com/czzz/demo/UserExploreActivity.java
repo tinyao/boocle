1f2bff22c17d5dc32ac509b7b3bb081e51bf0963
tinyao <yao2app@gmail.com>
2013-07-31 17:10:08 +0800

    调整Userlist卡片样式

--- boocle/src/com/czzz/demo/UserExploreActivity.java
+++ boocle/src/com/czzz/demo/UserExploreActivity.java
@@ -4,11 +4,16 @@ import java.util.ArrayList;
 
 import uk.co.senab.actionbarpulltorefresh.extras.actionbarsherlock.PullToRefreshAttacher;
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup.LayoutParams;
+import android.widget.AbsListView;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ListView;
 
 import com.actionbarsherlock.R.color;
@@ -20,6 +25,7 @@ import com.czzz.bookcircle.UserUtils;
 import com.czzz.demo.listadapter.DoubanRecommAdapter;
 import com.czzz.demo.listadapter.NearbyUsersAdapter;
 import com.czzz.utils.HttpListener;
+import com.czzz.view.LoadingFooter;
 
 import de.keyboardsurfer.android.widget.crouton.Crouton;
 import de.keyboardsurfer.android.widget.crouton.Style;
@@ -28,9 +34,11 @@ public class UserExploreActivity extends BaseActivity implements PullToRefreshAt
 
 	private static final int DEFAULT_NUM = 20;
 	private int usortId = 2;
+	private int school_id = User.getInstance().school_id;
 	
 	private PullToRefreshAttacher mPullToRefreshAttacher;
 	private ListView listView;
+	private LoadingFooter mLoadingFooter;
 	
 	private ArrayList<User> nearbyUsers = new ArrayList<User>();
 	private NearbyUsersAdapter nearbyUsersAdapter;
@@ -50,11 +58,68 @@ public class UserExploreActivity extends BaseActivity implements PullToRefreshAt
         listView = (ListView) findViewById(android.R.id.list);
         mPullToRefreshAttacher = PullToRefreshAttacher.get(this);
         mPullToRefreshAttacher.addRefreshableView(listView, this);
-        
+        mLoadingFooter = new LoadingFooter(this);
+	    listView.addFooterView(mLoadingFooter.getView());
+	    listView.addHeaderView(new View(this));
         
         fetchNeabyUsers(0, DEFAULT_NUM, User.getInstance().school_id, usortId);
+        
+        listView.setOnItemClickListener(new OnItemClickListener(){
+
+			@Override
+			public void onItemClick(AdapterView<?> arg0, View arg1, int pos,
+					long arg3) {
+				// TODO Auto-generated method stub
+				Intent goIntent = new Intent(UserExploreActivity.this, UserPageActivity.class);
+				User user = (User)nearbyUsersAdapter.getItem(pos-1);
+				goIntent.putExtra("user", user);
+				startActivity(goIntent);
+			}
+        	
+        });
+        
+        listView.setOnScrollListener(new AbsListView.OnScrollListener() {
+            @Override
+            public void onScrollStateChanged(AbsListView view, int scrollState) {
+            }
+
+            @Override
+            public void onScroll(AbsListView view, int firstVisibleItem, int visibleItemCount,
+                    int totalItemCount) {
+            	
+                if (mLoadingFooter.getState() == LoadingFooter.State.Loading
+                        || mLoadingFooter.getState() == LoadingFooter.State.TheEnd) {
+                    return;
+                }
+                if (firstVisibleItem + visibleItemCount >= totalItemCount
+                        && totalItemCount != 0
+                        && totalItemCount != listView.getHeaderViewsCount()
+                                + listView.getFooterViewsCount() && nearbyUsers.size() > 0) {
+                    
+                	Log.d("DEBUG", "load next page");
+                	loadNextPage();
+                }
+            }
+        });
 	}
 	
+	protected void loadNextPage() {
+		// TODO Auto-generated method stub
+		int startCid = 0;
+		if(usortId == 1){
+			// 最新用户
+			if(nearbyUsers != null && nearbyUsers.size() > 0){
+				startCid = nearbyUsers.get(nearbyUsers.size() - 1).uid;
+			}
+		}else{
+			// 藏书最多、随机排序  从
+			startCid = nearbyUsers.size();
+		}
+		mLoadingFooter.setState(LoadingFooter.State.Loading);
+		UserUtils.fetchNearbyUsers(startCid, DEFAULT_NUM, school_id, usortId, 
+				new UserExploreResponeHandler(this, HttpListener.FETCH_NEARBY_USERS_MORE));
+	}
+
 	/**
 	 * 获取同校书友
 	 * @param start
@@ -67,11 +132,11 @@ public class UserExploreActivity extends BaseActivity implements PullToRefreshAt
 		UserUtils.fetchNearbyUsers(start, count, school_id, usort, 
 				new UserExploreResponeHandler(this, HttpListener.FETCH_NEARBY_USERS));
 	}
-	
+    
 	@Override
 	public void onRefreshStarted(View view) {
 		// TODO Auto-generated method stub
-		
+		fetchNeabyUsers(0, DEFAULT_NUM, school_id, usortId);
 	}
 	
 	private void updateNearbyUsers(String response){
@@ -79,22 +144,24 @@ public class UserExploreActivity extends BaseActivity implements PullToRefreshAt
 		
 		if(users == null){
 			Crouton.makeText(this, R.string.no_more, Style.ALERT).show();
-			if(nearbyUsers.size() == 0){
-				nearbyUsersAdapter = new NearbyUsersAdapter(this, nearbyUsers);
-				listView.setAdapter(nearbyUsersAdapter);
-			}
+			mLoadingFooter.setState(LoadingFooter.State.TheEnd);
 		}else{
 			if(nearbyUsersAdapter == null || nearbyUsers.size() == 0){
 				nearbyUsers.addAll(users);
 				nearbyUsersAdapter = new NearbyUsersAdapter(this, nearbyUsers);
 				listView.setAdapter(nearbyUsersAdapter);
 			} else {
-//				morePd.setVisibility(View.GONE);
 				nearbyUsers.addAll(users);
-				nearbyUsersAdapter = new NearbyUsersAdapter(this, nearbyUsers);
 				nearbyUsersAdapter.notifyDataSetChanged();  
 			}
+			
+			if(users.size() < DEFAULT_NUM){
+				mLoadingFooter.setState(LoadingFooter.State.TheEnd);
+			}else{
+				mLoadingFooter.setState(LoadingFooter.State.Idle, 3000);
+			}
 			users.clear();
+			
 		}
 	}
 	
@@ -119,10 +186,12 @@ public class UserExploreActivity extends BaseActivity implements PullToRefreshAt
 			case HttpListener.FETCH_NEARBY_USERS:
 				nearbyUsers.clear();
 				updateNearbyUsers(response);
+				if(mPullToRefreshAttacher.isRefreshing()) 
+					mPullToRefreshAttacher.setRefreshComplete();
 				break;
-//			case HttpListener.FETCH_NEARBY_USERS_MORE:
-//				updateNearbyUsers(response);
-//				break;
+			case HttpListener.FETCH_NEARBY_USERS_MORE:
+				updateNearbyUsers(response);
+				break;
 			}
 		}
 		
